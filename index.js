1232b446777ea96e185ab231ff748132efc3fd62
Kumar Vikramjeet <vik001ind@gmail.com>
2015-07-31 04:24:26 +0000

    sasd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World121!')
+//  response.send('Hello World121!')
   var fs = require('fs');
   var buf1 = fs.readFileSync("index.html")
   response.send(buf1.toString('utf-8'))
